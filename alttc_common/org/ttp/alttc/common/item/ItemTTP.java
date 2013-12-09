a2625663c1aedbdf9cbe8fd88f8d8c767d8451c0
Jezzadabomb <jezzadabomb@gmail.com>
2013-12-09 18:40:38 +1100

    Fixed item registering.

--- alttc_common/org/ttp/alttc/common/item/ItemTTP.java
+++ alttc_common/org/ttp/alttc/common/item/ItemTTP.java
@@ -4,12 +4,19 @@ import net.minecraft.item.Item;
 
 import org.ttp.alttc.ALttC;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+
 public class ItemTTP extends Item{
 
     public ItemTTP(int id, String name) {
         super(id);
         setUnlocalizedName(name);
         setCreativeTab(ALttC.creativeTab);
+        register(name);
+    }
+    
+    public void register(String name){
+        GameRegistry.registerItem(this, name);
     }
 
 }
