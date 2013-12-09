6c256e5ebf964836d00f7922d9a91fa07c66590a
tterrag1098 <tterrag1098@gmail.com>
2013-12-09 06:52:44 -0600

    Fix ALL errors and warnings because OCD

--- alttc_common/org/ttp/alttc/client/renderer/ItemMasterSwordRenderer.java
+++ alttc_common/org/ttp/alttc/client/renderer/ItemMasterSwordRenderer.java
@@ -1,5 +1,7 @@
 package org.ttp.alttc.client.renderer;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.IItemRenderer;
 
@@ -10,78 +12,78 @@ import org.ttp.alttc.common.lib.TextureMaps;
 
 public class ItemMasterSwordRenderer implements IItemRenderer {
 
-    private ModelMasterSword modelMasterSword;
+	private ModelMasterSword modelMasterSword;
 
-    public ItemMasterSwordRenderer() {
-        modelMasterSword = new ModelMasterSword();
-    }
+	public ItemMasterSwordRenderer() {
+		modelMasterSword = new ModelMasterSword();
+	}
 
-    @Override
-    public boolean handleRenderType(ItemStack item, ItemRenderType type) {
-        return true;
-    }
+	@Override
+	public boolean handleRenderType(ItemStack item, ItemRenderType type) {
+		return true;
+	}
 
-    @Override
-    public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
-        return true;
-    }
+	@Override
+	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
+		return true;
+	}
 
-    @Override
-    public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
-        switch (type) {
-        case ENTITY: {
-            renderMasterSword(0F, 0.5F, 0F, 0.4F, false, false, false);
-            return;
-        }
+	@Override
+	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
+		switch (type) {
+		case ENTITY: {
+			renderMasterSword(0F, 0.5F, 0F, 0.4F, false, false, false);
+			return;
+		}
 
-        case EQUIPPED_FIRST_PERSON: {
-            renderMasterSword(0F, 1.0F, 0.4F, 0.5F, true, false, false);
-            break;
-        }
+		case EQUIPPED_FIRST_PERSON: {
+			renderMasterSword(0F, 1.0F, 0.4F, 0.5F, true, false, false);
+			break;
+		}
 
-        case INVENTORY: {
-            renderMasterSword(0F, 0.1F, 0.0F, 0.4F, false, true, false);
-            return;
-        }
+		case INVENTORY: {
+			renderMasterSword(0F, 0.1F, 0.0F, 0.4F, false, true, false);
+			return;
+		}
 
-        case EQUIPPED: {
-            renderMasterSword(0.5F, 0.5F, 0.4F, 0.5F, false, false, true);
-            return;
-        }
+		case EQUIPPED: {
+			renderMasterSword(0.5F, 0.5F, 0.4F, 0.5F, false, false, true);
+			return;
+		}
 
-        default:
-            return;
-        }
-    }
-    
-    private void renderMasterSword(float x, float y, float z, float scale, boolean fp, boolean inv, boolean eq) {
+		default:
+			return;
+		}
+	}
 
-        //TextureManager texture = Minecraft.getMinecraft().renderEngine;
+	private void renderMasterSword(float x, float y, float z, float scale, boolean fp, boolean inv, boolean eq) {
 
-        GL11.glPushMatrix();
-        GL11.glDisable(GL11.GL_LIGHTING);
+		TextureManager texture = Minecraft.getMinecraft().renderEngine;
 
-        GL11.glTranslatef(x, y, z);
-        GL11.glScalef(scale, scale, scale);
+		GL11.glPushMatrix();
+		GL11.glDisable(GL11.GL_LIGHTING);
 
-        if (fp)
-//            GL11.glTranslatef(0F, -0.20F, -0.17F);
-            
-        if (inv)
-//            GL11.glTranslatef(-0.01F, 0.12F, 0.0F);
-            
-        if (eq)
-//            GL11.glTranslatef(-0.22F, 0F, 0F);
+		GL11.glTranslatef(x, y, z);
+		GL11.glScalef(scale, scale, scale);
 
-        // Translate, texture, and render the electrons. Theoretically I can
-        // easily add electrons.
-//        GL11.glRotatef((float)angle, 0.0F, 1.0F, 0.0F);
-        RenderUtils.bindTexture(TextureMaps.MASTER_SWORD);
-        modelMasterSword.renderAll();
+		if (fp)
+			//            GL11.glTranslatef(0F, -0.20F, -0.17F);
 
-        GL11.glEnable(GL11.GL_LIGHTING);
-        GL11.glPopMatrix();
+		if (inv)
+			//            GL11.glTranslatef(-0.01F, 0.12F, 0.0F);
 
-    }
+		if (eq)
+			//            GL11.glTranslatef(-0.22F, 0F, 0F);
+
+		// Translate, texture, and render the electrons. Theoretically I can
+			// easily add electrons.
+			//        GL11.glRotatef((float)angle, 0.0F, 1.0F, 0.0F);
+			RenderUtils.bindTexture(TextureMaps.MASTER_SWORD);
+		modelMasterSword.renderAll();
+
+		GL11.glEnable(GL11.GL_LIGHTING);
+		GL11.glPopMatrix();
+
+	}
 
 }
