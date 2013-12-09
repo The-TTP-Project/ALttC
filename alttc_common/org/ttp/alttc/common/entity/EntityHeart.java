39551e5406ba4c72a0e938e5231f568ff3e5d6f6
tterrag1098 <tterrag1098@gmail.com>
2013-12-09 03:51:36 -0500

    Hearts functional, but still need to drop

--- alttc_common/org/ttp/alttc/common/entity/EntityHeart.java
+++ alttc_common/org/ttp/alttc/common/entity/EntityHeart.java
@@ -2,6 +2,7 @@ package org.ttp.alttc.common.entity;
 
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
@@ -11,12 +12,36 @@ public class EntityHeart extends EntityItem {
 	public EntityHeart(World par1World, double par2, double par4, double par6,
 			ItemStack par8ItemStack) {
 		super(par1World, par2, par4, par6, par8ItemStack);
+		
+		this.delayBeforeCanPickup = 40;
 	}
 	
 	@Override
-	public void onCollideWithPlayer(EntityPlayer par1EntityPlayer) {
+	public void onUpdate() {
+		delayBeforeCanPickup--;
 		
-		par1EntityPlayer.heal(1.0F);
+		super.onUpdate();
+	}
+
+	@Override
+	public void onCollideWithPlayer(EntityPlayer player) {
+		
+		if (this.delayBeforeCanPickup > 0)
+			return;
+		
+		for (ItemStack stack : player.inventory.mainInventory)
+		{
+			if (stack == null)
+				break;
+			
+			player.heal(2.0F);
+			
+			this.setDead();
+			return;
+		}
+		
+		super.onCollideWithPlayer(player);
+			
 	}
 
 }
--- alttc_common/org/ttp/alttc/common/item/ItemHeart.java
+++ alttc_common/org/ttp/alttc/common/item/ItemHeart.java
@@ -1,9 +1,40 @@
 package org.ttp.alttc.common.item;
 
+import org.ttp.alttc.common.entity.EntityHeart;
+import org.ttp.alttc.common.lib.ItemIds;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemHeart extends ItemTTP {
 
 	public ItemHeart(int id, String name) {
 		super(id, name);
-	}	
+	}
+
+	@Override
+	public void onUpdate(ItemStack par1ItemStack, World par2World,
+			Entity entity, int par4, boolean par5) {
+
+		if (entity instanceof EntityPlayer) {
+			System.out.println(ItemIds.ITEM_HEART);
+			((EntityPlayer) entity).inventory
+					.consumeInventoryItem(ItemIds.ITEM_HEART + 256);
+			
+			((EntityPlayer) entity).heal(1.0F);
+		}
+	}
+	
+	@Override
+	public Entity createEntity(World world, Entity location, ItemStack itemstack) {
+		
+		return new EntityHeart(world, location.posX, location.posY, location.posZ, itemstack);
+	}
+	
+	@Override
+	public boolean hasCustomEntity(ItemStack stack) {
+		return true;
+	}
 }
